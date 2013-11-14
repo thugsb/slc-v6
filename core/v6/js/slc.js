301a15d50ee9a02c03a48bdba7c0fba1efccf666
Stu Basden <stu@tapiocacollective.com>
2013-11-14 16:19:59 -0500

    revert mobile to the same-ish ui, style up some tablet and add fonts etc

--- core/v6/js/slc.js
+++ core/v6/js/slc.js
@@ -1,58 +1,94 @@
 jQuery(function($) {
   
+  
   window.$showcase = $('.showcase .tiles');
+  $showcase.isotope({
+    layoutMode: 'masonryHorizontal',
+    masonryHorizontal : {
+      rowHeight : 160
+    }
+  });
+  
   $(window).setBreakpoints({
     distinct: true,
-    breakpoints: [1, 768]
+    breakpoints: [1, 768, 992]
+  });
+  
+  $(window).bind('enterBreakpoint992',function() {
+  
+    // Adjust panel heights
+    $('.info-panel').height($(window).height() - 559)
+    
   });
   
   $(window).bind('enterBreakpoint768',function() {
     console.log('enter desktop');
-    if ($('.showcase .tiles').hasClass('cycle') ) {
-      $showcase.cycle('destroy').removeClass('cycle');
-    }
     
-    $showcase.isotope({
-      layoutMode: 'masonryHorizontal',
-      masonryHorizontal : {
-        rowHeight : 160
-      }
-    });
+    // Adjust panel heights
+    $('.slc-news .info-panel, .slc-spotlight .info-panel').height('auto');
+    $('.slc-sites .info-panel').height(10 + $('.slc-news .info-panel').height() + $('.slc-spotlight').height());
     
+    $('.showcase-left').off('click');
+    $('.showcase-right').off('click');
     $('.showcase-left').on('click', function() {
       $('.showcase-content').animate({scrollLeft: '-=480px'})
     });
-    
     $('.showcase-right').on('click', function() {
       $('.showcase-content').animate({scrollLeft: '+=480px'})
     });
+    
     
     $('header .info-panel').appendTo('.slc-sites');
   });
   
   $(window).bind('enterBreakpoint1',function() {
     console.log('enter mobile');
-    if ($('.showcase .tiles').hasClass('isotope') ) {
-      $showcase.isotope('destroy');
-    }
+    
     
     $('.showcase-left').off('click');
     $('.showcase-right').off('click');
-    
-    $showcase.find('.tile').each(function() {
-      var tileCSS = '{"top": "'+(Math.floor(Math.random()*21)-10)+'px", "left": "'+(Math.floor(Math.random()*21)-10)+'px", "transform": "rotate('+(Math.floor(Math.random()*21)-10)+'deg)"}';
-      $(this).attr('data-cycle-slide-css', tileCSS);
+    $('.showcase-left').on('click', function() {
+      $('.showcase-content').animate({scrollLeft: '-=300px'})
+    });
+    $('.showcase-right').on('click', function() {
+      $('.showcase-content').animate({scrollLeft: '+=300px'})
     });
     
-    $showcase.addClass('cycle').cycle({
-      slides: "> .tile",
-      swipe: true,
-      fx: "shuffle",
-      prev: '.showcase-left',
-      next: '.showcase-right'
-    });
     
     $('.slc-sites .info-panel').appendTo('.nav');
+  });
+  
+  
+  
+  
+  // Filter and search
+  $('#q').keyup(function(e) {
+    $('.slc-sites .info-panel a').removeClass('hidden');
+    $('.slc-sites .info-panel').removeHighlight();
+    var q = $(this).val().toLowerCase();
+    
+    if (q.length >= 2) {
+      $('.slc-sites .info-panel').addClass('filtering');
+      $('.slc-sites .info-panel a').addClass('hidden');
+      $('.slc-sites .info-panel ul ul a').each(function(i,v) {
+        if ($(this).text().toLowerCase().indexOf(q) > -1) {
+          $(this).removeClass('hidden');
+        }
+      });
+      $('.slc-sites .info-panel > ul > li').each(function(i,v) {
+        if ($(this).find('ul a').not('.hidden').length > 0) {
+          $(this).find('> a').removeClass('hidden');
+        }
+      });
+      $('.slc-sites .info-panel > ul > li > a').each(function(i,v) {
+        if ($(this).text().toLowerCase().indexOf(q) > -1) {
+          $(this).removeClass('hidden');
+        }
+      });
+      $('.slc-sites .info-panel').highlight(q);
+    } else {
+      $('.slc-sites .info-panel').removeClass('filtering');
+    }
   });
   
   
--- index.php
+++ index.php
@@ -16,6 +16,8 @@
   <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
   <link rel="stylesheet" href="core/v6/css/slc.css">
   <script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)</script>
+  <script type="text/javascript" src="//use.typekit.net/lqz8wak.js"></script>
+  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
   <!--[if lt IE 9]><script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.3.0/respond.min.js"></script><![endif]-->
 </head>
 <body class="center <?php if ($_GET['init']) {echo 'initialize';}?>">
@@ -40,13 +42,13 @@
   </header>
   
   <div class="intro-home"></div>
-  <div class="intro"><div class="intro-text">SARAH&middot;LAWRENCE&middot;COLLEGE</div></div>
+  <div class="intro"><div class="intro-text"><img src="img/sarah.png" alt="SARAH" class="intro-word"/><img src="img/dot.png" alt=" " class="intro-dot"/><img src="img/lawrence.png" alt="LAWRENCE" class="intro-word"/><img src="img/dot.png" alt=" " class="intro-dot"/><img src="img/college.png" alt="COLLEGE" class="intro-word"/></div></div>
   
   <section class="showcase">
     <div class="showcase-wrap">
       <h2>Explore.</h2>
-      <div class="showcase-scroll showcase-left">&lt;</div>
-      <div class="showcase-scroll showcase-right">&gt;</div>
+      <div class="showcase-scroll showcase-left"></div>
+      <div class="showcase-scroll showcase-right"></div>
       <div class="showcase-content">
         <div class="tiles clearfix" data-cycle-hide-non-active="false">
           <a data-cycle-timeout="8000" href="#" class="tile tile-big"><img src="home/1.jpg" class="img-responsive"/></a>
@@ -71,13 +73,13 @@
   
   <section class="navigate">
     <div class="bar row">
-      <nav class="portal-links col-sm-4 clearfix">
+      <nav class="portal-links col-md-4 col-sm-5 clearfix">
         <a href="/explore/" class="visible-xs">Explore. SLC.</a>
         <a href="/inquire/">Inquire</a>
         <a href="/visit/">Visit</a>
         <a href="/apply/">Apply</a>
       </nav>
-      <nav class="social-links col-sm-4">
+      <nav class="social-links col-md-4 col-sm-4">
         <a target="_blank" href="http://www.facebook.com/sarahlawrencecollege"><i class="fa fa-facebook"></i></a>
         <a target="_blank" href="http://www.twitter.com/SarahLawrence"><i class="fa fa-twitter"></i></a>
         <a target="_blank" href="#"><i class="fa fa-google-plus"></i></a>
@@ -85,9 +87,9 @@
         <a target="_blank" href="http://www.youtube.com/sarahlawrencecollege"><i class="fa fa-youtube"></i></a>
         <a href="tel:+1-914-337-0700"><i class="fa fa-phone"></i></a>
       </nav>
-      <nav class="explore-links col-sm-4 clearfix">
-        <a href="/explore/map/"><i class="fa fa-map-marker"></i> Campus Map</a>
-        <a href="/explore/360/">Virtual Tour</a>
+      <nav class="explore-links col-md-4 col-sm-3 clearfix">
+        <a href="/explore/map/"><i class="fa fa-map-marker"></i> <span class="explore-link-text">Campus Map</span></a>
+        <a href="/explore/360/"><img src="img/360-icon.png" alt=" "/> <span class="explore-link-text">Virtual Tour</span></a>
       </nav>
     </div>
   </section>
@@ -98,33 +100,166 @@
   
   <section class="info">
     <div class="row">
-      <div class="col-sm-4">
+      <div class="col-md-4 col-sm-6">
         <section class="slc-sites">
           <h4>SLC <strong>Sites.</strong></h4>
           <div class="info-panel">
             <ul>
               <li>
-                <a href="#/undergraduate">Undergraduate College</a>
+                <a href="#">Undergraduate College</a>
                 <ul>
-                  <li><a href="#/home">Home</a></li>
-                  <li><a href="#/home">Admission and Aid</a></li>
-                  <li><a href="#/home">Academics</a></li>
-                  <li><a href="#/home">Faculty</a></li>
-                  <li><a href="#/home">Study Abroad</a></li>
-                  <li><a href="#/home">Student Life</a></li>
-                  <li><a href="#/home">NCAA Athletics</a></li>
+                  <li><a href="#">Admission</a></li>
+                  <li><a href="#">Academics</a></li>
+                  <li><a href="#">Study Abroad Programs</a></li>
+                  <li><a href="#">Student Life</a></li>
+                  <li><a href="#">Athletics</a></li>
                 </ul>
               </li>
-              <li><a href="#/undergraduate">Graduate Programs</a></li>
-              <li><a href="#/undergraduate">Continuing Education</a></li>
-              <li><a href="#/undergraduate">High School Programs</a></li>
-              <li><a href="#/undergraduate">Adult Non-Credit Programs</a></li>
-              <li><a href="#/undergraduate">Guest Programs</a></li>
-              <li><a href="#/undergraduate">Athletics</a></li>
+              <li>
+                <a href="#">Graduate Programs</a>
+                <ul>
+                  <li><a href="#">MS Ed in the Art of Teaching</a></li>
+                  <li><a href="#">MS in Child Development</a></li>
+                  <li><a href="#">MSW/MA in Social Work and Child Development</a></li>
+                  <li><a href="#">MFA in Dance</a></li>
+                  <li><a href="#">MS in Dance/Movement Therapy</a></li>
+                  <li><a href="#">MA in Health Advocacy</a></li>
+                  <li><a href="#">MS in Human Genetics</a></li>
+                  <li><a href="#">MFA in Theatre</a></li>
+                  <li><a href="#">MA in Women’s History</a></li>
+                  <li><a href="#">MA/Juris Doctorate in Women’s History and Law</a></li>
+                  <li><a href="#">MFA in Writing</a></li>
+                  <li><a href="#">Graduate Admission and Aid</a></li>
+                  <li><a href="#">Student Life</a></li>
+                  
+                </ul>
+              </li>
+              <li>
+                <a href="#">Continuing Education</a>
+                <ul>
+                  <li><a href="#">Adult Programs</a></li>
+                  <li><a href="#">Children's Programs</a></li>
+                  <li><a href="#">The Writing Institute</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">High School and Pre-College Programs</a>
+              </li>
+              <li>
+                <a href="#">Adult Non-Credit Programs</a>
+                <ul>
+                  <li><a href="#">The Writing Institute</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Guest Programs</a>
+                <ul>
+                  <li><a href="#">Study Abroad Programs</a></li>
+                  <li><a href="#">Exchange Programs</a></li>
+                  <li><a href="#">Spring Writing in NYC</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Children's Programs</a>
+                <ul>
+                  <li><a href="#">Early Childhood Center</a></li>
+                  <li><a href="#">Swimming</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Athletics</a>
+                <ul>
+                  <li><a href="#">Go Gryphons!</a></li>
+                  <li><a href="#">Men’s Basketball</a></li>
+                  <li><a href="#">Men’s Crew</a></li>
+                  <li><a href="#">Women’s Crew</a></li>
+                  <li><a href="#">Men’s Cross Country</a></li>
+                  <li><a href="#">Women’s Cross Country</a></li>
+                  <li><a href="#">Men’s Equestrian</a></li>
+                  <li><a href="#">Women’s Equestrian</a></li>
+                  <li><a href="#">Men’s Soccer</a></li>
+                  <li><a href="#">Women’s Soccer</a></li>
+                  <li><a href="#">Women’s Swimming</a></li>
+                  <li><a href="#">Men’s Tennis</a></li>
+                  <li><a href="#">Women’s Tennis</a></li>
+                  <li><a href="#">Men’s Volleyball</a></li>
+                  <li><a href="#">Women’s Volleyball</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Visiting Campus</a>
+                <ul>
+                  <li><a href="#">Schedule a visit with Undergraduate Admission</a></li>
+                  <li><a href="#">Schedule a visit with Graduate Admission</a></li>
+                  <li><a href="#">Directions to Campus</a></li>
+                  <li><a href="#">Campus Map</a></li>
+                  <li><a href="#">Virtual Tour</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">SLC Community</a>
+                <ul>
+                  <li><a href="#">Academic Calendar</a></li>
+                  <li><a href="#">Alumni Community</a></li>
+                  <li><a href="#">Campus Map</a></li>
+                  <li><a href="#">Campus Safety and Security</a></li>
+                  <li><a href="#">Career Counseling</a></li>
+                  <li><a href="#">Financial Aid</a></li>
+                  <li><a href="#">Giving to SLC</a></li>
+                  <li><a href="#">Human Resources</a></li>
+                  <li><a href="#">Main Library</a></li>
+                  <li><a href="#">MySLC (Students, Faculty, Staff)</a></li>
+                  <li><a href="#">Parents</a></li>
+                  <li><a href="#">Student Accounts</a></li>
+                  <li><a href="#">Webmail</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Local Community</a>
+                <ul>
+                  <li><a href="#">Facilities</a></li>
+                  <li><a href="#">InTouch newsletter</a></li>
+                  <li><a href="#">Events Calendar</a></li>
+                  <li><a href="#">CE and Non-Credit Programs</a></li>
+                  <li><a href="#">Early Childhood Center</a></li>
+                  <li><a href="#">Campus Map</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Centers and Institutes</a>
+                <ul>
+                  <li><a href="#">Center for Continuing Education</a></li>
+                  <li><a href="#">The Writing Institute</a></li>
+                  <li><a href="#">The Child Development Institute</a></li>
+                  <li><a href="#">The Early Childhood Center</a></li>
+                  <li><a href="#">NYU Silver School of Social Work?</a></li>
+                  <li><a href="#">IFI?</a></li>
+                  <li><a href="#">White Mountain?</a></li>
+                  <li><a href="#">Beczak Environmental Education Center?</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Directories</a>
+                <ul>
+                  <li><a href="#">Faculty</a></li>
+                  <li><a href="#">Staff</a></li>
+                  <li><a href="#">Offices and Services</a></li>
+                  <li><a href="#">SLC Sites Directory</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Social Media</a>
+                <ul>
+                  <li><a href="#">Facebook</a></li>
+                  <li><a href="#">Twitter</a></li>
+                  <li><a href="#">YouTube</a></li>
+                  <li><a href="#">More…</a></li>
+                </ul>
+              </li>
             </ul>
             <form class="info-search form-inline clearfix">
               <div class="input-group">
-                <input type="text" class="form-control" placeholder="filter these categories or search"/>
+                <input type="text" class="form-control" id="q" placeholder="filter these categories or search"/>
                 <a class="input-group-addon"><i class="fa fa-filter"></i></a>
               </div>
               <button type="submit" class="btn btn-link"><i class="fa fa-search"></i></buttom>
@@ -133,7 +268,7 @@
         </section>
       </div>
       
-      <div class="col-sm-4">
+      <div class="col-md-4 col-sm-6">
         <section class="slc-news">
           <h4>SLC <strong>News and Events.</strong></h4>
           <div class="info-panel">
@@ -151,14 +286,14 @@
         </section>
       </div>
       
-      <div class="col-sm-4">
+      <div class="col-md-4 col-sm-6">
         <section class="slc-spotlight">
           <h4>SLC <strong>Spotlight.</strong></h4>
           <div class="info-panel">
             <div class="cycle-slideshow" data-cycle-slides="> .item" data-cycle-prev=".slc-spotlight .cycle-prev" data-cycle-next=".slc-spotlight .cycle-next">
               <div class="cycle-pager"></div>
-              <div class="cycle-control cycle-prev">&lt;</div>
-              <div class="cycle-control cycle-next">&gt;</div>
+              <div class="cycle-control cycle-prev"></div>
+              <div class="cycle-control cycle-next"></div>
               <div class="item">
                 <div class="spotlight-top"><img src="img/spotlight/urban.jpg"/></div>
                 <a class="spotlight-bottom">
@@ -203,6 +338,7 @@
   <script src="core/v6/js/jquery.isotope.js"></script>
   <script src="core/v6/js/breakpoints.js"></script>
   <script src="core/v6/js/jquery.color.js"></script>
+  <script src="core/v6/js/jquery.highlight.js"></script>
   <script src="core/v6/js/jquery.cycle2.js"></script>
   <script src="core/v6/js/jquery.cycle2.shuffle.js"></script>
   <script src="core/v6/js/jquery.cycle2.swipe.js"></script>
