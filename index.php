6321652cc76c7feed77632445a1e4c1196f233e1
Stu Basden <stu@tapiocacollective.com>
2013-11-27 13:10:36 -0500

    fix the header movement off-right and modal (modals have to be outside .wrap for mobile)

--- index.php
+++ index.php
@@ -85,23 +85,6 @@
             <div class="tile tile-big"><img src="home/4.jpg" class="img-responsive"/></div>
             <div class="tile"><img src="home/5.jpg" class="img-responsive"/></div>
           </div>
-          <div class="modal fade" id="asd">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                  <h4 class="modal-title">Modal title</h4>
-                </div>
-                <div class="modal-body">
-                  <p>One fine body&hellip;</p>
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                  <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-              </div><!-- /.modal-content -->
-            </div><!-- /.modal-dialog -->
-          </div><!-- /.modal -->
         </div>
       </div>
     </section>
@@ -388,7 +371,25 @@
       </div>
     </footer>
 
-  </div>
+  </div><!-- .wrap -->
+  
+  <div class="modal fade" id="asd">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          <h4 class="modal-title">Modal title</h4>
+        </div>
+        <div class="modal-body">
+          <p>One fine body&hellip;</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+  </div><!-- /.modal -->
 
   <div class="menu menu-left">
     <div class="menu-nav"><h4 class="pull-right">Explore.</h4><a class="link-home" href="/"><i class="fa fa-home"></i> www.slc.edu</a></div>
